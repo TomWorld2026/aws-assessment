515c5c2a2b059b0522019cac1aa491aaec4b3c16
Thomas Yang <thomas.yang2@gmail.com>
2026-03-11 16:20:13 +1100

    cognito client

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -5,8 +5,27 @@ resource "aws_cognito_user_pool" "pool" {
 }
 
 resource "aws_cognito_user_pool_client" "client" {
+
   name         = "candidate-client"
   user_pool_id = aws_cognito_user_pool.pool.id
+
+  generate_secret = false
+
+  access_token_validity  = 60
+  id_token_validity      = 60
+  refresh_token_validity = 30
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "days"
+  }
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH"
+  ]
 }
 
 resource "aws_cognito_user" "user" {
