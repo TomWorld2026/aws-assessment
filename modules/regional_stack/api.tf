783fc6261989b930554d9efb6ef5d765588869b8
Thomas Yang <thomas.yang2@gmail.com>
2026-03-11 16:07:46 +1100

    policy

--- modules/regional_stack/api.tf
+++ modules/regional_stack/api.tf
@@ -25,17 +25,51 @@ resource "aws_apigatewayv2_route" "greet" {
   authorizer_id = aws_apigatewayv2_authorizer.cognito.id
 }
 
-resource "aws_kms_key" "kms" {
-  description = "KMS encryption key"
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "log_key" {
+  description             = "KMS key for API Gateway logs"
   deletion_window_in_days = 7
   enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+
+      {
+        Sid = "EnableRootPermissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = "kms:*"
+        Resource = "*"
+      },
+
+      {
+        Sid = "AllowCloudWatchLogs"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.us-east-1.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_cloudwatch_log_group" "api_logs" {
   name              = "/aws/apigateway/candidate-api-${var.region}"
   retention_in_days = 7
 
-  kms_key_id = aws_kms_key.kms.arn
+  kms_key_id = aws_kms_key.log_key.arn
 }
 
 resource "aws_apigatewayv2_stage" "default" {
