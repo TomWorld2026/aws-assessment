6c9a446219026330a9bec8194478760ad1f1d86b
Thomas Yang <thomas.yang2@gmail.com>
2026-03-11 15:30:43 +1100

    stage access log

--- modules/regional_stack/api.tf
+++ modules/regional_stack/api.tf
@@ -25,12 +25,31 @@ resource "aws_apigatewayv2_route" "greet" {
   authorizer_id = aws_apigatewayv2_authorizer.cognito.id
 }
 
+resource "aws_cloudwatch_log_group" "api_logs" {
+  name              = "/aws/apigateway/candidate-api-${var.region}"
+  retention_in_days = 7
+}
+
 resource "aws_apigatewayv2_stage" "default" {
 
   api_id = aws_apigatewayv2_api.api.id
   name   = "$default"
 
   auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_logs.arn
+
+    format = jsonencode({
+      requestId      = "$context.requestId"
+      sourceIp       = "$context.identity.sourceIp"
+      requestTime    = "$context.requestTime"
+      httpMethod     = "$context.httpMethod"
+      routeKey       = "$context.routeKey"
+      status         = "$context.status"
+      responseLength = "$context.responseLength"
+    })
+  }  
 }
 
 resource "aws_apigatewayv2_authorizer" "cognito" {
